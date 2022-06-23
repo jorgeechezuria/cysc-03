2052e419ce19c429ce1f0887dbd57a0556cca5c5
Jorge Luis Echezuria Gonzalez <81248178+jorgeechezuria@users.noreply.github.com>
2022-06-23 11:27:23 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,13 +10,22 @@ typedef struct {
   float lm35;
   float pote;
 } temperature_data_t;
-
+#define PICO_DEFAULT_LED_PIN    25
+#define PICO_DEFAULT_LED_PIN1 6
+#define PICO_DEFAULT_LED_PIN2 7;
 /* Queue para comunicar los dos nucleos */
 queue_t queue;
 
 /* Main para el core 1 */
-void core1_main() {
-  
+void core1_main() {6
+// asigno un pin al pwm
+gpio_set_function(PICO_DEFAULT_LED_PIN1, GPIO_FUNC_PWM);
+gpio_set_function(PICO_DEFAULT_LED_PIN2, GPIO_FUNC_PWM);
+uint slice_num = pwm_gpio_to_slice_num(PICO_DEFAULT_LED_PIN);
+// configuro la velocidad de lectura 
+pwm_config_set_clkdiv(&config, 4.f);
+// inicio el pwm
+pwm_init(slice_num, &config, true);
     while(1) {
         /* Variable para recuperar el dato de la queue */
         temperature_data_t data;
@@ -24,6 +33,32 @@ void core1_main() {
         queue_remove_blocking(&queue, &data);
         printf("Temperatura actual: %.2f\n", data.lm35);
         printf("Temperatura deseada: %2f\n", data.pote);
+        float TempX = data.lm35-data.pote;
+
+        if(TempX<-10){
+            TempX = -10
+        }
+        if(TempX>10){
+            TempX = 10
+        }
+
+        float PWM = (TempX*65536)/10
+
+        if(TempX<0){
+            // Enciendo El Caloventor y Apago el aire acc
+            pwm_set_gpio_level(PICO_DEFAULT_LED_PIN6, 0); 
+            pwm_set_gpio_level(PICO_DEFAULT_LED_PIN7, PWM);
+        }else
+        if(TempX>0){
+            // Enciendo Aire acc y Apago el Caloventor
+            pwm_set_gpio_level(PICO_DEFAULT_LED_PIN6, PWM);
+            pwm_set_gpio_level(PICO_DEFAULT_LED_PIN7, 0);
+        }else
+        if(TempX==0){
+            //Apago Todo
+            pwm_set_gpio_level(PICO_DEFAULT_LED_PIN6, 0);
+            pwm_set_gpio_level(PICO_DEFAULT_LED_PIN7, 0);
+        }
     }
 }    
 
@@ -42,7 +77,9 @@ int main() {
     while(1) {
         /* Variable para enviar los datos */
         temperature_data_t data;
+        //Elijo canal del adc
         adc_select_input(0);
+        //Saco los Valores del adc
         uint16_t Lectura = adc_read();
         float Volt = (Lectura*3.3)/4095;
         float Temp = (Volt*1)/0.01;
@@ -50,6 +87,7 @@ int main() {
         uint16_t LPote = adc_read();
         float Potenciometro = (LPote*35.0/4095.0);
         /* Cuando los datos estan listos, enviar por la queue */
+        //preparo los valores para mandarlos al otro nucleo
         data.lm35 = Temp;
         data.pote = Potenciometro;
         queue_add_blocking(&queue, &data);
